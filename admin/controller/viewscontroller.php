98d3480b6ed4c8e6a1b325e49e6fec9af7eb32b3
romixar <you@example.com>
2017-02-25 06:30:59 +0300

    делаю шаблонизатор на PHP

--- admin/controller/viewscontroller.php
+++ admin/controller/viewscontroller.php
@@ -3,23 +3,37 @@
 
 class ViewsController{
     
+    public $data = []; // переменные для передачи в шаблон
     
-    public function __construct(){
-        
-        include '/../view/index.php';
-        
-    }
+//    public function __construct(){
+//        
+//        include '/../view/index.php';
+//        
+//    }
     
     public function render($tmpl){
         
-        //include '/admin/view/'.$tmpl.'.php';
-        include '/../view/'.$tmpl.'.php';
         
+        
+        ob_start();
+        extract($this->data); // названия ключей будут переменными
+        
+        //include '/admin/view/'.$tmpl.'.php';
+        include '/../view/'.$tmpl.'_tpl.php';
+        
+        return ob_get_clean($tmpl);
+        
+    }
+    
+    public function display($tmpl){
+        echo $this->render($tmpl);
     }
     
     
     
-    
+    public function set($k,$v){
+        $this -> data[$k] = $v;
+    }
     
 }
 
